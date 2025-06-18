38eab0d6c05b88004e548a5497757adeab2dd07f
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-18 20:38:14 -0300

    release: 0.1.9

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_codexproitec';
-$plugin->release = '0.1.8';
-$plugin->version = 2025_06_18_08;
+$plugin->release = '0.1.9';
+$plugin->version = 2025_06_18_09;
 $plugin->requires = 2022_11_28_06;
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = [
