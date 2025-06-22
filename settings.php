151450454acdc458d3b9a3f184c0e009a9fcc017
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-21 22:58:01 -0300

    fix: o settings só deve aparecer se estiver em modo admin

--- settings.php
+++ settings.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
-    $settings = new admin_settingpage('mod_codexproitec_settings', new lang_string('pluginname', 'mod_codexproitec'));
 
     // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
     if ($ADMIN->fulltree) {
+        // $settings = new admin_settingpage('mod_codexproitec_settings', new lang_string('pluginname', 'mod_codexproitec'));
         // TO-DO: Define actual plugin settings page and add it to the tree - {@link https://docs.moodle.org/dev/Admin_settings}.
     }
 }
