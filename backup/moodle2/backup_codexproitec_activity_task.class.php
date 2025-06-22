e0a357baf49ee388597d8e6a5607d5dffa4aad8d
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-21 22:58:12 -0300

    fix: não necessita corrigir os links

--- backup/moodle2/backup_codexproitec_activity_task.class.php
+++ backup/moodle2/backup_codexproitec_activity_task.class.php
@@ -28,25 +28,25 @@ defined('MOODLE_INTERNAL') || die();
 // More information about the backup process: {@link https://docs.moodle.org/dev/Backup_API}.
 // More information about the restore process: {@link https://docs.moodle.org/dev/Restore_API}.
 
-require_once($CFG->dirroot.'/mod/codexproitec/backup/moodle2/backup_codexproitec_stepslib.php');
-require_once($CFG->dirroot.'/mod/codexproitec/backup/moodle2/backup_codexproitec_settingslib.php');
+require_once($CFG->dirroot . '/mod/codexproitec/backup/moodle2/backup_codexproitec_stepslib.php');
+require_once($CFG->dirroot . '/mod/codexproitec/backup/moodle2/backup_codexproitec_settingslib.php');
 
 /**
  * Provides all the settings and steps to perform a complete backup of mod_codexproitec.
  */
-class backup_codexproitec_activity_task extends backup_activity_task {
+class backup_codexproitec_activity_task extends backup_activity_task
+{
 
     /**
      * Defines particular settings for the plugin.
      */
-    protected function define_my_settings() {
-        return;
-    }
+    protected function define_my_settings() {}
 
     /**
      * Defines particular steps for the backup process.
      */
-    protected function define_my_steps() {
+    protected function define_my_steps()
+    {
         $this->add_step(new backup_codexproitec_activity_structure_step('codexproitec_structure', 'codexproitec.xml'));
     }
 
@@ -56,19 +56,8 @@ class backup_codexproitec_activity_task extends backup_activity_task {
      * @param string $content
      * @return string
      */
-    public static function encode_content_links($content) {
-        global $CFG;
-
-        $base = preg_quote($CFG->wwwroot, '/');
-
-        // Link to the list of choices.
-        $search = "/(".$base."\//mod\/codexproitec\/index.php\?id\=)([0-9]+)/";
-        $content = preg_replace($search, '$@codexproitecINDEX*$2@$', $content);
-
-        // Link to choice view by moduleid.
-        $search = "/(".$base."\//mod\/codexproitec\/view.php\?id\=)([0-9]+)/";
-        $content = preg_replace($search, '$@codexproitecVIEWBYID*$2@$', $content);
-
+    public static function encode_content_links($content)
+    {
         return $content;
     }
 }
