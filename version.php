0a9f0bf9023b49efcede5ffc8c5b7036c8dcd6b7
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-23 16:03:43 -0300

    feat: [add] codex final

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_codexproitec';
-$plugin->release = '0.1.12';
-$plugin->version = 2025062112;
+$plugin->release = '0.1.13';
+$plugin->version = 2025062113;
 $plugin->requires = 2022112806;
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = [
